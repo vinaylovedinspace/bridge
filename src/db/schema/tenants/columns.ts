fbb5f450070645fe5625cfe72b39bfbef54ed07d
Vinay Nair <vinay.nair@avodah.com>
2025-09-23 15:40:50 +0530

    feat: remove required for whatsapp number

--- src/db/schema/tenants/columns.ts
+++ src/db/schema/tenants/columns.ts
@@ -4,7 +4,7 @@ export const TenantTable = pgTable('tenant', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: text('name').notNull(),
   ownerId: text('owner_id').notNull(),
-  whatsappNumber: text('whatsapp_number').notNull(),
+  whatsappNumber: text('whatsapp_number'),
 
   createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
   updatedAt: timestamp('updated_at', { mode: 'date' }).defaultNow().notNull(),
