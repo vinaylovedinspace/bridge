161f43cb68c987015f91210dcc20688d38339b87
Vinay Nair <vinay.nair@avodah.com>
2025-10-12 21:58:23 +0530

    chore: remove upstash

--- src/lib/redis.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '@/env';
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-  url: env.UPSTASH_REDIS_REST_URL,
-  token: env.UPSTASH_REDIS_REST_TOKEN,
-});
-
-export { redis };
